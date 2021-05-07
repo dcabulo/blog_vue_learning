23e6b19493352fc83c12d9a0c447a044ee0e94d4
dcabulo <diego.cabulo@linktic.co>
2021-05-06 23:24:48 -0500

    forms and inputs class

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,12 @@
 <template>
-  <h1>Reaction Timer</h1>
-  <button @click="start" :disabled="isPlaying">Play</button>
-  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
-  <Results v-if="showResults" :score="score" />
+  <SignUpForm />
 </template>
 
 <script>
-import Block from "./components/Block";
-import Results from "./components/Results";
+import SignUpForm from "./components/SignUpForm";
 export default {
   name: "App",
-  components: { Block, Results },
-  data() {
-    return {
-      isPlaying: false,
-      delay: null,
-      score: null,
-      showResults: false,
-    };
-  },
-  methods: {
-    start() {
-      this.delay = 2000 + Math.random() * 5000;
-      this.isPlaying = true;
-      this.showResults = false;
-    },
-    endGame(reactionTime) {
-      this.score = reactionTime;
-      this.isPlaying = false;
-      this.showResults = true;
-    },
-  },
+  components: { SignUpForm },
 };
 </script>
 
@@ -43,19 +19,8 @@ export default {
   color: #444;
   margin-top: 60px;
 }
-button {
-  background: #0faf87;
-  color: white;
-  border: none;
-  padding: 8px 16px;
-  border-radius: 4px;
-  font-size: 16px;
-  letter-spacing: 1px;
-  cursor: pointer;
-  margin: 10px;
-}
-button[disabled] {
-  opacity: 0.2;
-  cursor: not-allowed;
+body {
+  margin: 0;
+  background: #eee;
 }
 </style>
